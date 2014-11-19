800c7548cc782d32de5a8a40d609571a5200e15a
mTonkov <mtonkov@yahoo.com>
2014-11-20 00:01:00 +0200

    Initial commit - LoginRegisterPage

---
+++ ChildTracker/ChildTracker.Shared/ViewModels/LoginSignupViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ChildTracker.ViewModels
+{
+    public class LoginSignupViewModel : ViewModelBase
+    {
+        public UserViewModel User { get; set; }
+        public string Username { get; set; }
+        private ICommand signUpCommand;
+        private ICommand loginChildDevice;
+
+        public LoginSignupViewModel()
+        {
+            this.User = new UserViewModel();
+        }
+
+        public ICommand SignUpCommand
+        {
+            get
+            {
+                if (this.signUpCommand==null)
+                {
+                    this.signUpCommand = new RelayCommand(PerformSignup);
+                }
+                return this.signUpCommand;
+            }           
+        }
+        
+        public ICommand LoginChildDevice
+        {
+            get
+            {
+                if (this.loginChildDevice == null)
+                {
+                    this.loginChildDevice = new RelayCommand(PerformChildLogin);
+                }
+                return this.loginChildDevice;
+            }           
+        }
+
+        private void PerformChildLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PerformSignup()
+        {
+            var username = User.Username;
+            var pass = User.Password;
+            var b = 5;
+        }
+
+    }
+}
